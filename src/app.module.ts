36bcbd5b0df7320d8c0b1be1f167031cf1c48c2d
Lukasz Momot <lukasz.momot@hotmail.com>
2020-05-12 23:19:19 +0200

    Add stock price controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StockPriceController } from './stock-price/stock-price.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, StockPriceController],
   providers: [AppService],
 })
 export class AppModule {}
