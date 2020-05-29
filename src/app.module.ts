6ac95c54334820f6ef330864c2f3f71c62cad67f
Lukas Momot <lukasz.momot@hotmail.com>
2020-05-29 14:10:23 +0200

    Use stocks feature module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
-import { Module, HttpModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StocksController } from './stocks/stocks.controller';
-import { ConfigModule } from '@nestjs/config';
-import { StocksService } from './stocks/stocks.service';
+import { StocksModule } from './stocks/stocks.module';
 
 @Module({
-  imports: [HttpModule, ConfigModule.forRoot()],
-  controllers: [AppController, StocksController],
-  providers: [AppService, StocksService],
+  imports: [StocksModule],
+  controllers: [AppController],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/stocks/stocks.module.ts
@@ -0,0 +1,11 @@
+import { Module, HttpModule } from '@nestjs/common';
+import { StocksController } from './stocks.controller';
+import { StocksService } from './stocks.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+    imports: [HttpModule, ConfigModule.forRoot()],
+    controllers: [StocksController],
+    providers: [StocksService]
+})
+export class StocksModule { }
