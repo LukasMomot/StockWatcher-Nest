027016c6c867e6669cbe03997f5a84753d40b810
Lukas Momot <lukasz.momot@hotmail.com>
2020-07-09 15:03:50 +0200

    Add more most traded stocks

--- src/stocks/stocks.controller.ts
+++ src/stocks/stocks.controller.ts
@@ -7,28 +7,35 @@ import { Company } from 'src/models/api/company';
 
 @Controller('stocks')
 export class StocksController {
-  constructor(private readonly stockService: StocksService) { }
+  constructor(private readonly stockService: StocksService) {}
 
   @Get(':symbol/price')
-  public async getStockPrice(@Param('symbol') symbol: string): Promise<StockPrice> {
-    const result = await this.stockService.getStockPrice(symbol)
-      .pipe(map(mapToStockPrice)
-      ).toPromise();
+  public async getStockPrice(
+    @Param('symbol') symbol: string,
+  ): Promise<StockPrice> {
+    const result = await this.stockService
+      .getStockPrice(symbol)
+      .pipe(map(mapToStockPrice))
+      .toPromise();
 
     return result;
   }
 
   @Get('mosttraded')
   public getMostTradedStocks(): Promise<StockPrice[]> {
-    const mosttraded = ['AAPL', 'FB'];
+    // You can use stock from this list: https://www.investing.com/equities/most-active-stocks
+    const mosttraded = ['AAPL', 'FB', 'TWTR', 'NFLX', 'SHOP'];
 
-    return this.stockService.getStockPrices(mosttraded)
-      .pipe(map(stocks => stocks.map(mapToStockPrice))).toPromise();
-
+    return this.stockService
+      .getStockPrices(mosttraded)
+      .pipe(map(stocks => stocks.map(mapToStockPrice)))
+      .toPromise();
   }
 
   @Get(':symbol/company')
-  public async getCompanyInfo(@Param('symbol') symbol: string): Promise<Company> {
+  public async getCompanyInfo(
+    @Param('symbol') symbol: string,
+  ): Promise<Company> {
     const result = await this.stockService.getCompanyInfo(symbol).toPromise();
 
     return result;
