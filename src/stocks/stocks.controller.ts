f8cb173f7893eabe80ef028dcda9631c71470033
Lukas Momot <lukasz.momot@hotmail.com>
2020-11-12 11:29:14 +0100

    document the openapi

--- src/stocks/stocks.controller.ts
+++ src/stocks/stocks.controller.ts
@@ -4,12 +4,16 @@ import { map } from 'rxjs/operators';
 import { mapToStockPrice } from 'src/models/mappers/stock-price.mapper';
 import { StockPrice } from 'src/models/stockprice';
 import { Company } from 'src/models/api/company';
+import { ApiResponse, ApiOkResponse, ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('stocks')
 @Controller('stocks')
 export class StocksController {
-  constructor(private readonly stockService: StocksService) {}
+  constructor(private readonly stockService: StocksService) { }
 
   @Get(':symbol/price')
+  @ApiOkResponse({ status: 200, type: StockPrice })
+  @ApiOperation({ operationId: 'getStockPrice' })
   public async getStockPrice(
     @Param('symbol') symbol: string,
   ): Promise<StockPrice> {
@@ -22,6 +26,8 @@ export class StocksController {
   }
 
   @Get('mosttraded')
+  @ApiOkResponse({ status: 200, type: StockPrice, isArray: true })
+  @ApiOperation({ operationId: 'getMostTradedStocks' })
   public getMostTradedStocks(): Promise<StockPrice[]> {
     // You can use stock from this list: https://www.investing.com/equities/most-active-stocks
     const mosttraded = ['AAPL', 'FB', 'TWTR', 'NFLX', 'SHOP'];
